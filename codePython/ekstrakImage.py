ed8754b6ccbf9aa6060677cda429d8cdca38166f
Apriyanto Halim Ubuntu <apriyanto.halim@mikroskil.ac.id>
2024-07-01 10:00:54 +0700

    Mengekstrak gambar dari video

--- codePython/ekstrakImage.py
+++ codePython/ekstrakImage.py
@@ -1,4 +1,44 @@
 import cv2
-cam = cv2.VideoCapture('/home/apriyanto/Documents/video/video/archive/J-EDI20240607105034/2K0570OUTSV1019.mp4')
+import os
+
+##Get FPS from Video
+
+# Declar variabel for file name
+video = 'video name'
+
+# Get Video with opencv
+cam = cv2.VideoCapture(video)
+
+# Get FPS from video
 fps = cam.get(cv2.CAP_PROP_FPS)
-print(fps)
+
+print(f"FPS from video is = {fps}")
+
+##Convert Video to Image
+
+# Count the Image
+frame_count = 0
+
+# Get Video Name
+video_name = os.path.splitext(os.path.basename(video))[0]
+
+# Create an output folder with a name corresponding to the video
+output_directory = f"../../hasil_video/{video_name}_frames"
+os.makedirs(output_directory, exist_ok=True)
+
+while True:
+    ret, frame = cam.read()
+        
+    if not ret:
+        break
+        
+    frame_count += 1
+        
+    # Only extract frames at the desired frame rate
+    if frame_count % int(cam.get(5) / fps) == 0:
+        output_file = f"{output_directory}/frame_{frame_count}.jpg"
+        cv2.imwrite(output_file, frame)
+        print(f"Frame {frame_count} has been extracted and saved as {output_file}")
+    
+cam.release()
+cv2.destroyAllWindows()

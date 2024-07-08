78a1945163a432098cffcc90345ec0a5a141346b
Apriyanto Halim Ubuntu <apriyanto.halim@mikroskil.ac.id>
2024-07-08 14:11:22 +0700

    Menyesuaikan kode untuk melakukan proses alpha

--- codeDeblurImage/commandW.py
+++ codeDeblurImage/commandW.py
@@ -8,24 +8,24 @@ os.system("pwd")
 file_name = "codeDeblurImage/ezgif-frame-050.jpg"
 
 ## Directory Result Location
-res_loc = "hasil-lambda"
+res_loc = "hasil-alpha-deconv"
 
 # Making directory
-#os.makedirs(res_loc)
+os.makedirs(res_loc)
 
 ## Filename Result
 file_name_res = "result"
 
 ## Declare variabel
 com = [""] * 6
-com1 = [""] * 5
+com1 = [""] * 6
 
 #com[0] = "/usr/bin/bash"
 com[0] = "sudo"
 com1[0] = "sudo"
 
 ## run the iteration
-for i in range(1, 21):
+for i in range(1, 51, 2):
     ## example command
     ## ./estimate-kernel 15 hollywood.jpg kernel.tif
     ## ./deconv hollywood.jpg kernel.tif deblurred.png
@@ -34,10 +34,10 @@ for i in range(1, 21):
 
     ## Estimate Kernel
     com[1] = "codeDeblurImage/estimate-kernel" # command estimate kernel
-    com[2] = str(11) # kernel size
+    com[2] = str(7) # kernel size
     com[3] = file_name # filename to process
     com[4] = res_loc + "/" + "kernel.tif" # kernel name and location directory
-    com[5] = "--lambda-ratio=" + str(i * 10 ** (-3))
+    com[5] = "--no-multiscale"
     
 
     # run the command
@@ -48,6 +48,7 @@ for i in range(1, 21):
     com1[2] = file_name # filename to process
     com1[3] = res_loc + "/" + "kernel.tif" # kernel name and location directory
     com1[4] = res_loc + "/" + file_name_res + "-" + str(i) + ".png" # result file
+    com1[5] = "--alpha=" + str(i)
 
     # run the command
     subprocess.run(com1)

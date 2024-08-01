d20c024985499ad87d004d140f04721194b7d2b0
apriyanto2804 <craziyuhuu@gmail.com>
2024-08-02 00:05:21 +0700

    Menambahkan proses deblurring image

--- codeDeblurImage/commandW.py
+++ codeDeblurImage/commandW.py
@@ -3,7 +3,7 @@ import os
 import glob
 
 ## Directory Location and filename image file
-file_name = "codeDeblurImage/ezgif-frame-050.jpg"
+#file_name = "codeDeblurImage/ezgif-frame-050.jpg"
 
 ## Directory Name [source]
 dir_name = "/home/apriyanto/Documents/DatasetBaru/test/images"
@@ -26,10 +26,30 @@ com1 = [""] * 6
 com[0] = "sudo"
 com1[0] = "sudo"
 
-
+i = 0
 for image_file in os.listdir(dir_name):
     if(image_file.endswith(".jpg")):
-        print(image_file)
+        
+        print(f"Proses Citra ke-{i} = {image_file}")
+        
+        #Estimate the kernel
+        file_name = dir_name + "/" + image_file
+        com[1] = "codeDeblurImage/estimate-kernel" # command estimate kernel
+        com[2] = str(7) # kernel size
+        com[3] = file_name # filename to process
+        com[4] = res_loc + "/" + "kernel-" + str(i) + ".tif" # kernel name and location directory
+        com[5] = "--no-multiscale"
+        subprocess.run(com)
+
+        ## Deblurring Image
+        com1[1] = "codeDeblurImage/deconv" # command deblurring image
+        com1[2] = file_name # filename to process
+        com1[3] = res_loc + "/" + "kernel-" + str(i) + ".tif" # kernel name and location directory
+        com1[4] = res_loc + "/" + file_name_res + "-" + str(i) + ".png" # result file
+        com1[5] = "--alpha=" + str(9)
+        subprocess.run(com1)
+        
+        i = i + 1
 
 ## Untuk pengujian
 '''
